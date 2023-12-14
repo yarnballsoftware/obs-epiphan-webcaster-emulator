e97f6d4df258251399d11a661974707db6c5d74e
Tim Sloane <tim.sloane.1979@gmail.com>
2023-12-14 14:56:59 -0500

    Hardcode Tab for toggle key; add 2nd scene to settings, though not yet implemented in toggle logic.

--- webcaster_emulator.lua
+++ webcaster_emulator.lua
@@ -2,7 +2,7 @@ obs = obslua
 
 last_scene = ""
 toggled_scene_name = ""
-hotkey_id = obs.OBS_INVALID_HOTKEY_ID
+hotkey_toggle = obs.OBS_INVALID_HOTKEY_ID
 hotkey_pressed = false
 
 function switch_scene(scene_name)
@@ -37,20 +37,30 @@ function toggle_scene(pressed)
 end
 
 function script_description()
-    return "Toggle between the currently active scene and a predefined scene."
+    return "Emulate the keyboard functionality of the Epiphan Webcaster X2 for picture-in-picture between two scenes/sources"
 end
 
 function script_properties()
     local props = obs.obs_properties_create()
 
     -- Dropdown for scene selection
-    local p = obs.obs_properties_add_list(props, "scene_list", "Select Scene", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    local p = obs.obs_properties_add_list(props, "scene_list", "Select Scene 1", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     local scenes = obs.obs_frontend_get_scenes()
     if scenes then
         for _, scene in ipairs(scenes) do
             local scene_name = obs.obs_source_get_name(scene)
             obs.obs_property_list_add_string(p, scene_name, scene_name)
             obs.obs_source_release(scene)
+        end
+    end
+
+    local p2 = obs.obs_properties_add_list(props, "scene_list2", "Select Scene 2", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    local scenes2 = obs.obs_frontend_get_scenes()
+    if scenes2 then
+        for _, scene2 in ipairs(scenes2) do
+            local scene_name = obs.obs_source_get_name(scene2)
+            obs.obs_property_list_add_string(p2, scene_name, scene_name)
+            obs.obs_source_release(scene2)
         end
     end
 
@@ -64,8 +74,8 @@ end
 
 function script_save(settings)
     obs.obs_data_set_string(settings, "last_scene", last_scene)
-    
-    local hotkey_save_data = obs.obs_hotkey_save(hotkey_id)
+
+    local hotkey_save_data = obs.obs_hotkey_save(hotkey_toggle)
     if hotkey_save_data then
         local wrapper = obs.obs_data_create()
         obs.obs_data_set_array(wrapper, "hotkey_data", hotkey_save_data)
@@ -77,13 +87,14 @@ function script_save(settings)
 end
 
 function script_load(settings)
-    hotkey_id = obs.obs_hotkey_register_frontend("toggle_scene_hotkey", "Toggle Scene Hotkey", toggle_scene)
-    
-    local json_str = obs.obs_data_get_string(settings, "toggle_scene_hotkey_data")
+    hotkey_toggle = obs.obs_hotkey_register_frontend(script_path(), "Toggle Scene Hotkey", toggle_scene)
+
+    local json_str = "{\"hotkey_data\":[{\"key\":\"OBS_KEY_TAB\"}]}"
     if json_str and json_str ~= "" then
         local wrapper = obs.obs_data_create_from_json(json_str)
         local hotkey_save_data = obs.obs_data_get_array(wrapper, "hotkey_data")
-        obs.obs_hotkey_load(hotkey_id, hotkey_save_data)
+        print("debug json_str: " .. json_str)
+        obs.obs_hotkey_load(hotkey_toggle, hotkey_save_data)
         obs.obs_data_array_release(hotkey_save_data)
         obs.obs_data_release(wrapper)
     end
